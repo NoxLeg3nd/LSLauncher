a632191c2249104a171aee7442ba135793690796
NoxLeg3nd <MihaiVasilescu03@gmail.com>
2025-08-26 21:50:30 +0300

    Added new files.

---
+++ LSLauncher/Views/UserControls/NavBar.axaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LSLauncher.Views.UserControls;
+
+public partial class NavBar : UserControl
+{
+    public NavBar()
+    {
+        InitializeComponent();
+    }
+    
+    
+}
