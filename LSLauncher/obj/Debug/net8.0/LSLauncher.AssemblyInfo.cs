3981d7d8abac41c0f572b21e07450c52c1fb1802
Ramazan Alkan <ramazan905tv@gmail.com>
2025-08-28 23:53:12 +0300

    Remove tracked build artifacts

--- LSLauncher/obj/Debug/net8.0/LSLauncher.AssemblyInfo.cs
+++ LSLauncher/obj/Debug/net8.0/LSLauncher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LSLauncher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bd17e18b6b9da4f3a023e92d0285279dda30435")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adfa05024390db47bbe86559522f31b370ddc578")]
 [assembly: System.Reflection.AssemblyProductAttribute("LSLauncher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LSLauncher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
